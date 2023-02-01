f89e6f6afb97326c9701a16a303876f5a0160392
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-02-01 13:44:17 -0800

    RGR passing for armor.js

--- __tests__/armor.test.js
+++ __tests__/armor.test.js
@@ -3,6 +3,6 @@ import Armor from './../src/js/armor.js';
 describe('Armor', () => {
     test('this test should create a piece of armor with its stats', () => {
         const angelArmor = new Armor("Angel's Armor", "Chestplate", ["elf", "female"], 0.83)
-        expect(angelArmor.name).toEqual("Devil's Armor");
+        expect(angelArmor.name).toEqual("Angel's Armor");
     });
   });
