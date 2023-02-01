460a01c283f4fb4894fbcb8796b68a6f4897b091
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-02-01 10:32:39 -0800

    RGR TEST WORKED WERE THE BEST HERO OBJECT DEFINED YESSSSS
    Co-authored-by: Aaron Demski
    <ajdemski@frontier.com>

--- __tests__/hero.test.js
+++ __tests__/hero.test.js
@@ -3,6 +3,14 @@ import Hero from './../src/js/hero.js';
 describe('Hero', () => {
   test('should create a hero with filled out attributes', () => {
       const hero = new Hero('Raina Lightfoot', 'Female', 'Elf', 'Archer', 10, 12, 18, 9)
-    expect(hero.name).toEqual('Chuck Norris');
+    expect(hero.name).toEqual('Raina Lightfoot');
+    expect(hero.sex).toEqual('Female');
+    expect(hero.race).toEqual('Elf');
+    expect(hero.job).toEqual('Archer');
+    expect(hero.strength).toEqual(10);
+    expect(hero.intelligence).toEqual(12);
+    expect(hero.dexterity).toEqual(18);
+    expect(hero.stamina).toEqual(9);
+    
   });
 });
--- src/js/hero.js
+++ src/js/hero.js
@@ -3,10 +3,10 @@ export default function Hero(name, sex, race, job, strength, intelligence, dexte
     this.sex = sex;
     this.race = race;
     this.job = job;
-    this.strength = {};
-    this.intelligence = {};
-    this.dexterity = {};
-    this.stamina = {};
+    this.strength = strength;
+    this.intelligence = intelligence;
+    this.dexterity = dexterity;
+    this.stamina = stamina;
   }
 
 
@@ -16,4 +16,5 @@ if race = elf, dexterity = dexterity * 1.25
 if race = dwarf, stamina = stamina * 1.5
 if race = gnome, intelligence = intelligence * 1.25
 
+can add inventory object and inject it into the Hero object, or items equipped object, or skills to perform object, etc. lots of things to do here, places to go, people to see.  you know, all that good stuff - slots, equipped stuff, etc.
 */
