52a63f0a3b8703e7e9c1e8b11066e968fc50ad24
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-02-01 12:01:50 -0800

    it passed yay

--- __tests__/weapon.test.js
+++ __tests__/weapon.test.js
@@ -3,6 +3,6 @@ import Weapon from './../src/js/weapon.js';
 describe('Weapon', () => {
   test('should create a weapon with filled out attributes', () => {
     const angelBow = new Weapon("Angel's Bow", "Bow", ["dexterity", "intelligence"], 2.43)
-    expect(angelBow.name).toEqual("Acid Dagger");
+    expect(angelBow.name).toEqual("Angel's Bow");
 });
 });
